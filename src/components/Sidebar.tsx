bda93d8777892655c34a48fe917399ff5cc293bd
sanjeev0303 <sanjeev.081021@gmail.com>
2024-09-29 06:08:24 +0530

    adding sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,52 @@
-import React from 'react'
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { sidebarLinks } from '@/constants';
+import { cn } from '@/lib/utils';
 
 const Sidebar = () => {
-  return (
-    <div className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+    const pathname = usePathname();
 
-<div className='flex flex-1 flex-col gap-6'>
+    console.log('Checkpoint 1: Before rendering the Sidebar component');
 
-</div>
+    return (
+      <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+        <div className="flex flex-1 flex-col gap-6">
+          {sidebarLinks.map((item) => {
+            const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
 
-    </div>
-  )
-}
+            console.log(`Checkpoint 2: Processing item ${item.label}`);
 
-export default Sidebar
+            return (
+              <Link
+                href={item.route}
+                key={item.label}
+                className={cn(
+                  'flex gap-4 items-center p-4 rounded-lg justify-start',
+                  {
+                    'bg-blue-1': isActive,
+                  }
+                )}
+              >
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  width={24}
+                  height={24}
+                />
+                <p className="text-lg font-semibold max-lg:hidden">
+                  {item.label}
+                </p>
+              </Link>
+            );
+          })}
+        </div>
+      </section>
+    );
+  };
+
+  console.log('Checkpoint 3: After defining the Sidebar component');
+
+  export default Sidebar;
