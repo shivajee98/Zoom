232b53cd6f103835619fa78304f64711c8c5ce62
sanjeev0303 <sanjeev.081021@gmail.com>
2024-09-29 09:58:28 +0530

    adding home page and cards

---
+++ src/components/MeetingTypeList.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+import HomeCard from "./HomeCard";
+import { useRouter } from "next/navigation";
+
+const MeetingTypeList = () => {
+
+    const router = useRouter()
+
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoingingMeeting" | "isInstantMeeting" | undefined
+  >();
+
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Stat an instant meeting."
+        handleClick={() => setMeetingState("isInstantMeeting")}
+        cardColor="bg-orange-1"
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting."
+        handleClick={() => setMeetingState("isScheduleMeeting")}
+        cardColor="bg-blue-1"
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Check out your recordings."
+        handleClick={() => router.push('/recordings') }
+        cardColor="bg-purple-1"
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="Via invitation link."
+        handleClick={() => setMeetingState("isJoingingMeeting") }
+        cardColor="bg-yellow-1"
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
