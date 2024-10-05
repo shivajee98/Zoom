8e58517f58e58ce932f559cdbb4eecb1726abfac
sanjeev0303 <sanjeev.081021@gmail.com>
2024-10-05 21:24:09 +0530

    adding meeting recordings

--- src/components/MeetingCard.tsx
+++ src/components/MeetingCard.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import { avatarImages } from "@/constants";
-// import { useToast } from "./ui/use-toast";
 import { useToast } from "@/hooks/use-toast";
 
 interface MeetingCardProps {
