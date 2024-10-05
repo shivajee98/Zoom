f40ff20133a8dfd51ae45934f91a01f3c46d8eef
sanjeev0303 <sanjeev.081021@gmail.com>
2024-10-06 04:49:47 +0530

    adding personal room

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -20,7 +20,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { LayoutList, User } from "lucide-react";
 import { Button } from "./ui/button";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import EndCallButton from "./EndCallButton";
 import Loader from "./Loader";
 
@@ -33,11 +33,13 @@ const MeetingRoom = () => {
   const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
   const [showParticipents, setShowParticipents] = useState(false);
 
-  const { useCallCallingState } = useCallStateHooks()
+  const { useCallCallingState } = useCallStateHooks();
 
-  const callingState = useCallCallingState()
+  const callingState = useCallCallingState();
 
-  if(callingState !== CallingState.JOINED) return <Loader />
+  const router = useRouter()
+
+  if (callingState !== CallingState.JOINED) return <Loader />;
 
   const CallLayout = () => {
     switch (layout) {
@@ -70,7 +72,9 @@ const MeetingRoom = () => {
       </div>
 
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
-        <CallControls />
+        <CallControls onLeave={() => {
+            router.push("/")
+        }} />
 
         <DropdownMenu>
           <div className="flex items-center">
